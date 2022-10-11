6aefe356825bc29b619495bad57831874ca62393
Mo Binglin <2756283851mo@gmail.com>
2022-10-11 19:44:08 +0800

    10.11.2

---
+++ build/vel_pkg_c/CMakeFiles/vel_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/aspopnly/Clone/catkin_ws/devel/lib/vel_pkg_c/vel_node"
+  "/home/aspopnly/Clone/catkin_ws/devel/lib/vel_pkg_c/vel_node.pdb"
+  "CMakeFiles/vel_node.dir/src/vel_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vel_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/vel_pkg_c/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aspopnly/Clone/catkin_ws/src/vel_pkg_c
+# Build directory: /home/aspopnly/Clone/catkin_ws/build/vel_pkg_c
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
